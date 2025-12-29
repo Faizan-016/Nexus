9336e5128797b0ea5db55d33af7a923885aa912d
Faizan <faizanzcloud@gmail.com>
2025-12-29 14:26:24 +0500

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,21 @@
+// const express =require('express');
+
+import express from 'express';
+import dotenv from 'dotenv';
+
+import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
+
+dotenv.config();
+// create the express instance
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// endpoints
+app.use("/api/auth", authRoutes),
+app.use("/api/message", messageRoutes)
+
+
+app.listen(PORT, () => console.log
+('Server running on port' + PORT));
