21c899ef22dfcb7de63bf4393d1b6c06ed6fe418
Faizan <faizanzcloud@gmail.com>
2025-12-30 00:39:30 +0500

    Welcome email implemented

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs"
+import { ENV } from "../lib/env.js"
 
 export const signup = async (req, res) => {
 
@@ -57,9 +59,15 @@ export const signup = async (req, res) => {
                 email: newUser.email,
                 profilePic: newUser.profilePic,
 
-            })
+            });
 
-            //todo: send an email to the user
+            try {
+                await sendWelcomeEmail(savedUser.email, savedUser.fullName, ENV.CLIENT_URL);
+            } catch (error) {
+                console.error("Failed to send welcome email:", error)
+            }
+
+
         } else {
             res.status(400).json({ message: "Invalid user data" })
         }
